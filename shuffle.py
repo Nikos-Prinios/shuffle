8c16b7446308679f21cd09ec5be166dcf9e2fa99
Nikos <habanita@gmail.com>
2015-05-23 10:46:40 +0200

    security

--- shuffle.py
+++ shuffle.py
@@ -2,7 +2,7 @@ import pickle, os, random, bpy
 
 # ************************************************************************************************
 # Define a pattern
-pattern = 'arnaud fireworks arnaud guitare remi tanguy remi guitare fireworks'
+pattern = 'tag1 tag3 tag1 tag1 tag1'
 
 # Pace of the music (in frames)
 global pace
@@ -44,18 +44,15 @@ def new_data(inpoint,outpoint):
 log_file = os.path.expanduser('~/%s.ez' % 'Easy-Logging-log-file')
 if os.path.exists(log_file):
 	path_list, user,log = pickle.load( open( log_file, "rb" ) )
-	print('Metadata created by : ' + user)
 	
 	# La list des tags
 	tag_list = set()
 	for t in pattern:
 		tag_list.add(t)
-	print ('tag list: ' + str(tag_list))
 	
 	# construction du dictionnaire des tags
 	for t in tag_list:	
 		dict[t]=[]
-		print(t)
 		for clip_file in log:
 			if len(clip_file) > 1 :
 				clip = clip_file[0]
@@ -69,43 +66,45 @@ if os.path.exists(log_file):
 						print(clip[0], inpoint, outpoint)
 
 	
+if len(dict[t]) > 0 :
+	start = bpy.context.scene.frame_current
+	begin_at = start
 
-start = bpy.context.scene.frame_current
-begin_at = start
+	original_type = bpy.context.area.type
+	bpy.context.area.type = "SEQUENCE_EDITOR"
 
-original_type = bpy.context.area.type
-bpy.context.area.type = "SEQUENCE_EDITOR"
+	while True:
+		# create the list of clips to import on the timeline	
+		list = []
+		for t in pattern:
+			try:
+				list.append( random.choice(dict[t]) )
+			except:
+				pass
+		# populating....
 
-while True:
-	# create the list of clips to import on the timeline	
-	list = []
-	for t in pattern:
-		try:
-			list.append( random.choice(dict[t]) )
-		except:
-			pass
-	# populating....	
-	for c in list:
-		clip = c[0]
-		inout = new_data(c[1],c[2])
-		inpoint = inout[0]
-		outpoint = inout[1]
-		path = find_path(clip)
-		
-	 	# the lego job
-		bpy.ops.sequencer.select_all(action = "DESELECT")
-		bpy.ops.sequencer.movie_strip_add(frame_start=start, filepath=path, sound=False, channel=1)
-		strip = bpy.context.scene.sequence_editor.active_strip
-		strip.frame_final_end=start+outpoint
-		strip.frame_final_start=start+inpoint
-		bpy.ops.transform.seq_slide(value=(-(strip.frame_final_start-start), 0))
-		start = strip.frame_final_end
-		bpy.context.scene.frame_current = start
+		if not list : break
+		for c in list:
+			clip = c[0]
+			inout = new_data(c[1],c[2])
+			inpoint = inout[0]
+			outpoint = inout[1]
+			path = find_path(clip)
+    			
+			# the lego job
+			bpy.ops.sequencer.select_all(action = "DESELECT")
+			bpy.ops.sequencer.movie_strip_add(frame_start=start, filepath=path, sound=False, channel=1)
+			strip = bpy.context.scene.sequence_editor.active_strip
+			strip.frame_final_end=start+outpoint
+			strip.frame_final_start=start+inpoint
+			bpy.ops.transform.seq_slide(value=(-(strip.frame_final_start-start), 0))
+			start = strip.frame_final_end
+			bpy.context.scene.frame_current = start
+			if start > (Total_length + begin_at):
+				break 
 		if start > (Total_length + begin_at):
-			break 
-	if start > (Total_length + begin_at):
-			break
-	
-bpy.context.scene.frame_end = start
-bpy.ops.sequencer.view_all()  
-bpy.context.area.type = original_type
+				break
+    		
+	bpy.context.scene.frame_end = start
+	bpy.ops.sequencer.view_all()  
+	bpy.context.area.type = original_type
